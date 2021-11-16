177efb9b25099f1d3a7569bf77bea51fc78915f5
JoachimStanislaus <Joachimtan00@gmail.com>
2021-11-16 10:45:44 +0800

    Added 2P and Bot Gamemode Selection

--- TicTacToe_bot.py
+++ TicTacToe_bot.py
@@ -2,7 +2,26 @@ import telebot
 
 bot = telebot.TeleBot('2142718885:AAFNwMfbBpKq9lrfEvydERsOn4RM5AhjiS8')
 
+def Pbot():
+    print("In PBot")
 
+def TwoP():
+    print("In 2P")
+
+#To Determine Gamemode 2P or Bot
+def TwoPorBot(message):
+    if message.text == "2P" :
+        TwoP()    
+    elif message.text == "Bot" :
+        Pbot()
+
+@bot.message_handler(commands=['startgame'])
+def start_game(message):
+    start_game_message = "Game will begin. \n 2P or Bot?"
+    start_markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    start_markup.row('2P',"Bot")
+    sent = bot.send_message(message.chat.id, start_game_message, reply_markup=start_markup)
+    bot.register_next_step_handler(sent,TwoPorBot)
 
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
